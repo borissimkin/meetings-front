4ce47381d6a3c155b1f7bbcb55e98db27938690e
boris <simkinbb@gmail.com>
2021-02-23 14:20:16 +0300

    Добавил доску

--- src/views/Room.vue
+++ src/views/Room.vue
@@ -1,8 +1,27 @@
 <template>
   <div class='room p-4'>
     <div>
-      <StreamingArea :room-id='id' />
-      <SettingsMediaDevices />
+
+      <v-tabs v-model='tab'
+              background-color='primary'
+              dark>
+
+        <v-tab>
+          Стримы
+        </v-tab>
+        <v-tab>
+          Доска
+        </v-tab>
+      </v-tabs>
+      <v-tabs-items v-model='tab'>
+        <v-tab-item>
+          <StreamingArea :room-id='id' />
+          <SettingsMediaDevices />
+        </v-tab-item>
+        <v-tab-item :eager='true'>
+          <Whiteboard :height='600' :width='900' style='min-width: 900px; min-height: 600px'></Whiteboard>
+        </v-tab-item>
+      </v-tabs-items>
     </div>
     <Chat />
   </div>
@@ -12,10 +31,12 @@
 import Chat from '@/components/Chat'
 import StreamingArea from '@/components/StreamingArea'
 import SettingsMediaDevices from '@/components/SettingMediaDevices'
+import Whiteboard from '@/components/Whiteboard'
 
 export default {
   name: 'Room',
   components: {
+    Whiteboard,
     SettingsMediaDevices,
     StreamingArea,
     Chat,
@@ -30,6 +51,7 @@ export default {
   data() {
     return {
       usersInChatRoom: [],
+      tab: null,
     }
   },
 
@@ -39,7 +61,7 @@ export default {
 
   beforeDestroy() {
     this.$socket.client.emit('disconnect')
-  }
+  },
 }
 </script>
 
